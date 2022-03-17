f31ccfae17df2532297df35d3464003fe28f2e96
mrAdiletDev <adiletdj19@gmail.com>
2022-03-17 16:53:12 +0600

    CREATE Card model

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    serial_number = models.IntegerField(max_length=999)
+    created_at = models.DateTimeField()
+    ends_at = models.DateTimeField()
+    amount = models.IntegerField()
+    status = models.CharField(
+        max_length=50,
+        choices=["not activated", "activated", "expired"]
+    )
